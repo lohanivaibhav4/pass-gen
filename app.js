a234c0dc1a1ec0e4cf7d96aab68d54780b4bd4ca
Vaibhav Lohani <lohanivaibhav4@gmail.com>
2024-11-08 19:05:08 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let pass1El = document.getElementById("pass1")
+let pass2El = document.getElementById("pass2")
+
+
+
+function generate(){
+    let pass1 ="passone"
+    let pass2 ="passtwo"
+    pass1El.textContent = pass1
+    pass2El.textContent = pass2
+}
