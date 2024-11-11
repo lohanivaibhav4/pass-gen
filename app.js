2c71cd8919a33fb9e24a64f03ee43c4f767bb9f3
Vaibhav Lohani <lohanivaibhav4@gmail.com>
2024-11-11 13:02:10 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 let pass1El = document.getElementById("pass1")
 let pass2El = document.getElementById("pass2")
 
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
+    "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+    "o","p","q","r","s","t","u","v","w","x","y","z",
+     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+     "~","`","!","@","#","$","%","^","&","*","(",")","_",
+     "-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 
+let passLength = 12;
+
+function getRandomCharacter(){
+    let randomChar = Math.floor(Math.random()*characters.length)
+    return characters[randomChar];
+}
+function generateRandomPassword(){
+    let pass = ""
+    
+    for (let i = 0; i < passLength; i++) {
+        pass += getRandomCharacter()         
+    }
+    return pass;
+}
 
 function generate(){
-    let pass1 ="passone"
-    let pass2 ="passtwo"
-    pass1El.textContent = pass1
-    pass2El.textContent = pass2
+    pass1El.textContent = generateRandomPassword();
+    pass2El.textContent = generateRandomPassword();
 }
+
+
